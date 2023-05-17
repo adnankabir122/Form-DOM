3a374f36a5f0762225004b1637b339065184fc04
adnankabir122 <adnankabir122@gmail.com>
2023-05-17 11:56:14 +0600

    up to new keyword

--- Object Oriented Javascript/properties.js
+++ Object Oriented Javascript/properties.js
@@ -1,3 +1,13 @@
+/* 
+Topics: 
+1. Create Object with some properties
+2. access the properties
+3. modify or change value of properties
+4. add new properties
+5. check if a property exists or not 
+6. iterate properties one by one using for..in loop 
+7. delete a property 
+*/ 
 
 // 1. create an object
 let person = {
@@ -35,3 +45,6 @@ for(let key in person){
     console.log(`${key} : ${person[key]}`)
     // dot notation ekhane(loop a) kaj kore na,bracket notation use korte hoy 
 }
+
+// 7. delete a property 
+delete person.age;
